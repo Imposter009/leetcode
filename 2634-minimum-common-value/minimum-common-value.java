22c71b87962b3c442fef61c8f6d4b5ca19a46579
Sumit Gupta <87868589+Imposter009@users.noreply.github.com>
2024-03-10 00:35:37 +0530

    Update minimum-common-value.java

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -17,3 +17,36 @@ class Solution {
     return ans;
     }
 }
+-------------------------------------------------------------------------------------------------------------------------------------------
+    // Approach-2 (Using Binary Search)
+// T.C : O(mlogn)
+// S.C : O(1)
+class Solution {
+    public boolean binarySearch(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public int getCommon(int[] nums1, int[] nums2) {
+        for (int num : nums1) {
+            if (binarySearch(nums2, num)) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
