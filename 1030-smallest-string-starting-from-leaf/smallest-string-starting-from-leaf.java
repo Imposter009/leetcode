9fe57ebf62806c4547160c83657bf5a16909f970
Sumit Gupta <87868589+Imposter009@users.noreply.github.com>
2024-04-18 01:47:51 +0530

    Update smallest-string-starting-from-leaf.java

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -39,3 +39,34 @@ class Solution {
         return result;
     }
 }
+
+
+-------------------------------------
+class Solution {
+    String res="";
+    public String ans(TreeNode node, String str)
+    {
+        if(node==null) return str;
+        String curr=(char) (node.val + 'a')+""+ str;
+        if(node.left==null && node.right==null) return curr;
+        String left=ans(node.left,curr);
+        String right=ans(node.right,curr);
+       
+        if(left.compareTo(right)<0)
+        {
+             if(left.compareTo(res)<0 || res.equals(""))
+            res=left;
+        }
+      if(right.compareTo(left)<0 )
+        {
+            if( right.compareTo(res)<0 || res.equals(""))
+            res=right;
+        }
+
+      return res;
+    }
+    public String smallestFromLeaf(TreeNode root) {
+       return ans(root,"");
+    }
+}
+// note: error came for : 0 null 1
